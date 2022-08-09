1f898fae6543463bca5b3bd5ce4bae72e7d43de8
aleksei@mazur <mazuralexey@gmail.com>
2022-08-09 23:50:39 +0300

    first steps

---
+++ store/runner.py
@@ -0,0 +1,19 @@
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from store.spiders.castorama import CastoramaSpider
+from scrapy.utils.log import configure_logging
+
+
+if __name__ == '__main__':
+    configure_logging()
+    settings = get_project_settings()
+    runner = CrawlerRunner(settings)
+
+    # query = input('Что ищем?: ')
+    # runner.crawl(CastoramaSpider, query=query)
+
+    runner.crawl(CastoramaSpider, query='мотоблок')
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()
---
+++ store/spiders/castorama.py
@@ -0,0 +1,15 @@
+import scrapy
+from scrapy.http import HtmlResponse
+
+
+class CastoramaSpider(scrapy.Spider):
+    name = 'castorama'
+    allowed_domains = ['castorama.ru']
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.start_urls = [f'https://www.castorama.ru/catalogsearch/result/?q={kwargs.get("query")}']
+
+
+    def parse(self, response: HtmlResponse):
+        print()
